f636c6881579739af9277748cf28988a68b7e588
Jimmy Paolini <jpaolini@joor.com>
2022-09-19 13:48:54 -0400

    feat: listElementCount option (#88)
    Add an option to configure how many list elements should be generated in mocks.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { ASTKindToNode, NamedTypeNode, parse, printSchema, TypeNode } from 'graphql';
+import { ASTKindToNode, ListTypeNode, NamedTypeNode, parse, printSchema, TypeNode } from 'graphql';
 import casual from 'casual';
 import { PluginFunction, oldVisit } from '@graphql-codegen/plugin-helpers';
 import { pascalCase } from 'pascal-case';
@@ -21,6 +21,7 @@ type Options<T = TypeNode> = {
     currentType: T;
     customScalars?: ScalarMap;
     transformUnderscore: boolean;
+    listElementCount?: number;
     dynamicValues?: boolean;
 };
 
@@ -216,11 +217,14 @@ const generateMockValue = (opts: Options): string | number | boolean => {
                 currentType: opts.currentType.type,
             });
         case 'ListType': {
-            const value = generateMockValue({
-                ...opts,
-                currentType: opts.currentType.type,
-            });
-            return `[${value}]`;
+            const listElements = Array.from({ length: opts.listElementCount }, (_, index) =>
+                generateMockValue({
+                    ...opts,
+                    fieldName: opts.fieldName + index,
+                    currentType: (opts.currentType as ListTypeNode).type,
+                }),
+            );
+            return `[${listElements.join(', ')}]`;
         }
         default:
             throw new Error('unreached');
@@ -326,6 +330,7 @@ export interface TypescriptMocksPluginConfig {
     typesPrefix?: string;
     enumsPrefix?: string;
     transformUnderscore?: boolean;
+    listElementCount?: number;
     dynamicValues?: boolean;
 }
 
@@ -365,6 +370,7 @@ export const plugin: PluginFunction<TypescriptMocksPluginConfig> = (schema, docu
     const enumValuesConvention = config.enumValues || 'pascal-case#pascalCase';
     const typenamesConvention = config.typenames || 'pascal-case#pascalCase';
     const transformUnderscore = config.transformUnderscore ?? true;
+    const listElementCount = config.listElementCount > 0 ? config.listElementCount : 1;
     // List of types that are enums
     const types: TypeItem[] = [];
     const visitor: VisitorType = {
@@ -407,6 +413,7 @@ export const plugin: PluginFunction<TypescriptMocksPluginConfig> = (schema, docu
                         currentType: node.type,
                         customScalars: config.scalars,
                         transformUnderscore,
+                        listElementCount,
                         dynamicValues: config.dynamicValues,
                     });
 
