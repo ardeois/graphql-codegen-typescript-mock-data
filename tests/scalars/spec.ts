1fdde47bf5e5b6b505cc4b94bd20ed24b7be1209
Ricardo Vela <ricardo.garza@betterment.com>
2025-04-01 13:06:38 -0600

    Add example coverage to scalars/spec.ts
    I.e. the very basic display of what this PR is meant to change. Follow
    up commits will cover the faker side of things, and possibly other
    config permutations.

--- tests/scalars/spec.ts
+++ tests/scalars/spec.ts
@@ -98,6 +98,131 @@ describe('Custom scalar generation using casual', () => {
 
         expect(result).toMatchSnapshot();
     });
+
+    describe('with different input/output configurations', () => {
+        it('should generate distinct custom scalars for native and custom input/output types', async () => {
+            const result = await plugin(testSchema, [], {
+                generateLibrary: 'casual',
+                scalars: {
+                    String: 'string',
+                    Float: {
+                        generator: 'double',
+                        arguments: [-100, 0],
+                    },
+                    ID: {
+                        generator: 'integer',
+                        arguments: [1, 100],
+                    },
+                    Boolean: 'false',
+                    Int: {
+                        generator: 'integer',
+                        arguments: [-100, 0],
+                    },
+                    AnyObject: {
+                        input: 'string',
+                        output: 'email',
+                    },
+                },
+            });
+
+            expect(result).toBeDefined();
+
+            // String
+            expect(result).toContain(
+                "str: overrides && overrides.hasOwnProperty('str') ? overrides.str! : 'ea corrupti qui incidunt eius consequatur blanditiis',",
+            );
+
+            // Float
+            expect(result).toContain(
+                "flt: overrides && overrides.hasOwnProperty('flt') ? overrides.flt! : -24.509902694262564,",
+            );
+
+            // ID
+            expect(result).toContain("id: overrides && overrides.hasOwnProperty('id') ? overrides.id! : 82,");
+
+            // Boolean
+            expect(result).toContain("bool: overrides && overrides.hasOwnProperty('bool') ? overrides.bool! : false");
+
+            // Int
+            expect(result).toContain("int: overrides && overrides.hasOwnProperty('int') ? overrides.int! : -93,");
+
+            // AnyObject in type A (an email)
+            expect(result).toContain(
+                "anyObject: overrides && overrides.hasOwnProperty('anyObject') ? overrides.anyObject! : 'Kelly_Cremin@Turcotte.biz',",
+            );
+
+            // AnyObject in input C (a string)
+            expect(result).toContain(
+                "anyObject: overrides && overrides.hasOwnProperty('anyObject') ? overrides.anyObject! : 'itaque distinctio iure molestias voluptas reprehenderit quos',",
+            );
+
+            expect(result).toMatchSnapshot();
+        });
+
+        it('should generate distinct dynamic custom scalars for native and custom types', async () => {
+            const result = await plugin(testSchema, [], {
+                generateLibrary: 'casual',
+                dynamicValues: true,
+                scalars: {
+                    String: 'string',
+                    Float: {
+                        generator: 'double',
+                        arguments: [-100, 0],
+                    },
+                    ID: {
+                        generator: 'integer',
+                        arguments: [1, 100],
+                    },
+                    Boolean: 'false',
+                    Int: {
+                        generator: 'integer',
+                        arguments: [-100, 0],
+                    },
+                    AnyObject: {
+                        input: 'string',
+                        output: 'email',
+                    },
+                },
+            });
+
+            expect(result).toBeDefined();
+
+            // String
+            expect(result).toContain(
+                "str: overrides && overrides.hasOwnProperty('str') ? overrides.str! : casual['string'],",
+            );
+
+            // Float
+            expect(result).toContain(
+                "flt: overrides && overrides.hasOwnProperty('flt') ? overrides.flt! : casual['double'](...[-100,0]),",
+            );
+
+            // ID
+            expect(result).toContain(
+                "id: overrides && overrides.hasOwnProperty('id') ? overrides.id! : casual['integer'](...[1,100]),",
+            );
+
+            // Boolean
+            expect(result).toContain("bool: overrides && overrides.hasOwnProperty('bool') ? overrides.bool! : false");
+
+            // Int
+            expect(result).toContain(
+                "int: overrides && overrides.hasOwnProperty('int') ? overrides.int! : casual['integer'](...[-100,0]),",
+            );
+
+            // AnyObject in type A (an email)
+            expect(result).toContain(
+                "anyObject: overrides && overrides.hasOwnProperty('anyObject') ? overrides.anyObject! : casual['email'],",
+            );
+
+            // AnyObject in input C (an string)
+            expect(result).toContain(
+                "anyObject: overrides && overrides.hasOwnProperty('anyObject') ? overrides.anyObject! : casual['string'],",
+            );
+
+            expect(result).toMatchSnapshot();
+        });
+    });
 });
 
 describe('custom scalar generation using faker', () => {
